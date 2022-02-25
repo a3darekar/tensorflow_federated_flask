d41f529a6bacb1df289e3b9051ac7cfeee17ea01
Amey Darekar <darekar.amey@gmail.com>
2022-02-25 17:22:46 +0200

    Updating file imports

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import os
 
 DEBUG = bool(os.getenv('DEBUG', False))
+INPUT_SHAPE = bool(os.getenv('INPUT_SHAPE', False))
+CLASS_COUNT = bool(os.getenv('CLASS_COUNT', False))
 APP_URL = str(os.getenv('ADMIN_URL', "http://127.0.0.1:5000"))
 
 NODE = str(os.getenv('NODE', 'EDGE')).lower()  # 'edge'
