a0c6397b23dd467e5ab0d17435133c5b3e78b4a2
Amey Darekar <darekar.amey@gmail.com>
2022-02-27 00:10:38 +0200

    Added Training and Evaluation definition to edge models

--- tf_federated/edge_model.py
+++ tf_federated/edge_model.py
@@ -5,7 +5,7 @@ from tensorflow.python.keras import models
 import numpy as np
 import tensorflow.python.keras.losses
 
-from keras_utils import set_model_weights, get_rid_of_the_models
+from .keras_utils import get_rid_of_the_models
 
 
 class Client:
@@ -15,23 +15,28 @@ class Client:
 		self.x_train = None
 		self.y_train = None
 
-	def _init_model(self, model_fn: Callable, model_weights=None):
+	def init_model(self, model_fn: Callable, model_weights=None):
 		model = model_fn()
 		if model_weights and os.path.isfile(model_weights):
-			set_model_weights(model, model_weights)
+			print("Loading model with saved weights")
+			weights = np.load(model_weights, allow_pickle=True)
+			model.set_weights(weights)
+		else:
+			print("Loading fresh model")
 		model.compile(
 			loss=tensorflow.keras.losses.categorical_crossentropy,
 			optimizer=tensorflow.keras.optimizers.Adadelta(),
 			metrics=['accuracy']
 			)
+		print(model.summary())
 		self.model = model
 
 	def receive_data(self, x, y):
-		self.x_train = x
+		self.x_train = x.astype("float32") / 255
 		self.y_train = y
 
 	def receive_and_init_model(self, model_fn: Callable, model_weights):
-		self._init_model(model_fn, model_weights)
+		self.init_model(model_fn, model_weights)
 
 	def edge_train(self, client_train_dict: dict):
 		if self.model is None:
@@ -52,5 +57,6 @@ class Client:
 
 	def save_local_weights(self):
 		weights = self.model.get_weights()
-		np.save('weights', weights)
+		np.save(f'weights_{self.client_id}', weights)
+		# np.save('weights', weights)
 		print("Saved Local weights")
