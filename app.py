6f4526a6b53024b82c82e96a85f664ff96eb1a8d
Amey Darekar <darekar.amey@gmail.com>
2022-02-27 00:04:29 +0200

    moving controller files outside tf package

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from setup import NODE, DEBUG, PORT, NODE_ID
 
 if NODE != 'edge':
 	print(f"STARTING AGGREGATOR NODE ON PORT: {PORT}")
-	from tf_federated.aggregator_controller import socket, app
+	from aggregator_controller import socket, app
 	socket.run(app, debug=DEBUG)
 else:
 	print(f"STARTING EDGE NODE: {NODE_ID}")
-	from tf_federated.edge_controller import run
+	from edge_controller import run
 	run()
