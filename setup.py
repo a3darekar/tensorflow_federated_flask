9e0a2bb56eadea34cbab66b61fb152d6e65f804c
Amey Darekar <darekar.amey@gmail.com>
2022-03-01 17:14:16 +0200

    Restructured setup script

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 import os
 
-DEBUG = bool(os.getenv('DEBUG', False))
-DATA_FILE = str(os.getenv('DATA_FILE', False))
-AGGR_MODEL_FILE = str(os.getenv('AGGR_MODEL_FILE', 'weights.npy'))
-INPUT_SHAPE = list(map(int, str(os.getenv('INPUT_SHAPE', False)).split()))
-CLASS_COUNT = int(os.getenv('CLASS_COUNT', False))
-APP_URL = str(os.getenv('ADMIN_URL', "http://127.0.0.1:5000"))
-
 NODE = str(os.getenv('NODE', 'EDGE')).lower()  # 'edge'
 NODE_ID = int(os.getenv('NODE_ID', 1))  # 'edge'
 if NODE != 'edge':
 	PORT = 5000  # 'admin'
 else:
 	PORT = 5000 + NODE_ID  # 'edge'
+
+DEBUG = bool(os.getenv('DEBUG', False))
+DATA_FILE = str(os.getenv('DATA_FILE', False))
+AGGR_MODEL_FILE = str(os.getenv('AGGR_MODEL_FILE', 'weights/weights.npy'))
+INPUT_SHAPE = list(map(int, str(os.getenv('INPUT_SHAPE', False)).split()))
+MODEL_WEIGHTS_FILE = str(os.getenv('MODEL_WEIGHTS_FILE', f"weights/weights_{NODE_ID}.npy"))
+CLASS_COUNT = int(os.getenv('CLASS_COUNT', False))
+APP_URL = str(os.getenv('ADMIN_URL', "http://127.0.0.1:5000"))
+
 
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
